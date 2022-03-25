4a65ea6b1dd5df8556bb76c5d0f79d00ac43b6bb
MichaelSandilands <michael.b.sandilands@gmail.com>
2022-03-25 15:09:50 +0000

    Added Functions: theme_msand(), scale_colour_msand(), scale_color_msand(), scale_fill_msand()

--- R/ggplot-scale_manual.R
+++ R/ggplot-scale_manual.R
@@ -32,3 +32,52 @@ palette_msand <- function() {
         toupper()
 
 }
+
+#' msand colors and fills for ggplot2.
+#'
+#' @description
+#' The msand scales add colors that work nicely with `theme_msand()`.
+#'
+#'  @details
+#' \describe{
+#'
+#' \item{`scale_colour_msand`}{
+#' For use when `colour` is specified as an `aes()` in a ggplot.}
+#'
+#' \item{`scale_fill_msand`}{
+#' For use when `fill` is specified as an `aes()` in a ggplot.}
+#' }
+#'
+#' @seealso [theme_msand()]
+#'
+#' @param ... common discrete scale parameters: `name`, `breaks`, `labels`, `na.value`, `limits` and `guide`. See [discrete_scale()] for more details
+#'
+#' @examples
+#' library(ggplot2)
+#'
+#' diamonds %>%
+#'     ggplot(aes(carat, price, colour = cut)) +
+#'     geom_point() +
+#'     scale_colour_msand() +
+#'     theme_msand()
+#'
+#' @name scale_manual
+NULL
+
+#'
+#' @rdname scale_manual
+#' @export
+
+scale_colour_msand <- function(...) {
+    ggplot2::scale_color_manual(values = unname(palette_msand()))
+}
+
+#' @rdname scale_manual
+#' @export
+scale_color_msand <- scale_colour_msand
+
+#' @rdname scale_manual
+#' @export
+scale_fill_msand <- function(...) {
+    ggplot2::scale_fill_manual(values = unname(palette_msand()))
+}
